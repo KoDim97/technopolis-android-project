f66fb5eda8a4bcd6c22d2c3190a209ed966faeeb
jhoysbou <neeroll21@gmail.com>
2020-04-27 23:07:18 +0300

    presenter changed

--- app/src/main/java/com/example/technopark/screens/profile/ProfilePresenter.java
+++ app/src/main/java/com/example/technopark/screens/profile/ProfilePresenter.java
@@ -1,18 +1,8 @@
 package com.example.technopark.screens.profile;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.technopark.BaseActivity;
-import com.example.technopark.R;
 import com.example.technopark.screens.common.mvp.MvpPresenter;
 
 public class ProfilePresenter implements MvpPresenter<ProfileMvpView>, ProfileMvpView.Listener {
-    private ClipboardManager myClipboard;
-    private ClipData myClip;
 
     @Override
     public void bindView(ProfileMvpView view) {
