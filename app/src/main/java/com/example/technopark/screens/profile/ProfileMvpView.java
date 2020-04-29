b8f541266c0b1c253777be9b786feb1f34530a52
jhoysbou <neeroll21@gmail.com>
2020-04-29 04:40:21 +0300

    databind method in progress

--- app/src/main/java/com/example/technopark/screens/profile/ProfileMvpView.java
+++ app/src/main/java/com/example/technopark/screens/profile/ProfileMvpView.java
@@ -5,10 +5,15 @@ import com.example.technopark.screens.common.mvp.MvpViewObservable;
 import com.example.technopark.screens.common.nav.BackPressedListener;
 
 public interface ProfileMvpView extends MvpViewObservable<ProfileMvpView.Listener> {
+
     interface Listener extends BackPressedListener {
 
         void onBtnGoBackClicked();
     }
+
+    void showProgress();
+
+    void hideProgress();
 
     void bindData(UserProfile userProfile);
 
