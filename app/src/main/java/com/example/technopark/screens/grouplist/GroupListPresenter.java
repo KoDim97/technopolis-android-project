a37c851b2c5ba8a248a3961e452ab463ef415c47
jhoysbou <neeroll21@gmail.com>
2020-04-30 13:03:35 +0300

    style fixed

--- app/src/main/java/com/example/technopark/screens/grouplist/GroupListPresenter.java
+++ app/src/main/java/com/example/technopark/screens/grouplist/GroupListPresenter.java
@@ -35,7 +35,8 @@ public class GroupListPresenter implements MvpPresenter<GroupListMvpView>,
         this.mainThreadPoster = mainThreadPoster;
     }
 
-    @Override public void bindView(GroupListMvpView view) {
+    @Override
+    public void bindView(GroupListMvpView view) {
         this.view = view;
         view.showProgress();
         loadItems();
@@ -57,24 +58,28 @@ public class GroupListPresenter implements MvpPresenter<GroupListMvpView>,
         view.bindData(groupItem);
     }
 
-    @Override public void onStart() {
+    @Override
+    public void onStart() {
         view.registerListener(this);
         backPressDispatcher.registerListener(this);
     }
 
-    @Override public void onStop() {
+    @Override
+    public void onStop() {
         view.unregisterListener(this);
         backPressDispatcher.unregisterListener(this);
     }
 
-    @Override public void onDestroy() {
+    @Override
+    public void onDestroy() {
         // dispose all requests
         thread.interrupt();
         thread = null;
         view = null;
     }
 
-    @Override public boolean onBackPressed() {
+    @Override
+    public boolean onBackPressed() {
         screenNavigator.navigateUp();
         return true;
     }
@@ -87,16 +92,16 @@ public class GroupListPresenter implements MvpPresenter<GroupListMvpView>,
             List<Student> filteredStudent = new ArrayList<>();
 
             if (text.length() != 0) {
-                for(Student student: students){
-                    if(student.getFullname().toLowerCase().contains(text.toLowerCase())){
+                for (Student student : students) {
+                    if (student.getFullname().toLowerCase().contains(text.toLowerCase())) {
                         filteredStudent.add(student);
                     }
                 }
-            }else{
+            } else {
                 filteredStudent = students;
             }
 
-            GroupItem filteredGroup = new GroupItem(groupItem.getId(), groupItem.getName(),filteredStudent);
+            GroupItem filteredGroup = new GroupItem(groupItem.getId(), groupItem.getName(), filteredStudent);
             if (!thread.isInterrupted()) {
                 mainThreadPoster.post(() -> onItemsLoaded(filteredGroup));
             }
