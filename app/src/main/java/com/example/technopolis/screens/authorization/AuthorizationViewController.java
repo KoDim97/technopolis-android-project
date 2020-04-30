0878613584ad30de7915949d1420ba46d4a25604
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2020-04-30 21:15:19 +0300

    back in menu

--- app/src/main/java/com/example/technopolis/screens/authorization/AuthorizationViewController.java
+++ app/src/main/java/com/example/technopolis/screens/authorization/AuthorizationViewController.java
@@ -67,8 +67,8 @@ public class AuthorizationViewController {
 
 
     private void test() {
-        loginEditText.setText("***REMOVED***");
-        passwordEditText.setText("***REMOVED***");
+        loginEditText.setText("");
+        passwordEditText.setText("");
     }
 
     private void changeEnableEnter() {
