bdfa966526e20ddb83b8fd61673a2172b0c81ac2
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2020-05-18 15:05:46 +0300

    refactor

--- app/src/main/java/com/example/technopolis/screens/common/download/ImageStorage.java
+++ app/src/main/java/com/example/technopolis/screens/common/download/ImageStorage.java
@@ -3,24 +3,27 @@ package com.example.technopolis.screens.common.download;
 import android.graphics.Bitmap;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 
 public class ImageStorage {
-    private HashMap<String, ImageTarget> images;
+    private final HashMap<String, ImageTarget> images;
 
     public ImageStorage() {
         images = new HashMap<>();
     }
 
-    public void setImage(Bitmap image, String imageUrl) {
+    public void setImage(@NonNull Bitmap image,@NonNull String imageUrl) {
         ImageTarget buf = new ImageTarget();
         buf.setImage(image);
         images.put(imageUrl, buf);
     }
 
-    public void downloadImage(String imageUrl, ImageView view) {
+    public void downloadImage(@NonNull String imageUrl,@NonNull ImageView view) {
         if (!images.containsKey(imageUrl)) {
             ImageTarget buf = new ImageTarget();
             buf.setView(view);
@@ -30,7 +33,8 @@ public class ImageStorage {
         }
     }
 
-    public Bitmap getImage(String imageUrl) {
+    @Nullable
+    public Bitmap getImage(@NonNull String imageUrl) {
         if (!images.containsKey(imageUrl))
             return null;
         return images.get(imageUrl).getImage();
--- app/src/main/java/com/example/technopolis/screens/common/download/ImageTarget.java
+++ app/src/main/java/com/example/technopolis/screens/common/download/ImageTarget.java
@@ -4,6 +4,9 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
@@ -12,11 +15,11 @@ public class ImageTarget implements Target {
     private Bitmap image;
     private ImageView view;
 
-    public void setView(ImageView view) {
+    public void setView(@NonNull ImageView view) {
         this.view = view;
     }
 
-    void setImage(Bitmap image) {
+    void setImage(@NonNull Bitmap image) {
         this.image = image;
     }
 
@@ -36,6 +39,7 @@ public class ImageTarget implements Target {
     public void onPrepareLoad(Drawable placeHolderDrawable) {
     }
 
+    @Nullable
     Bitmap getImage() {
         return image;
     }
