e1ce198447ca8fa62dd9016a8f0820ffd9900467
Ilya <gusarov2000@gmail.com>
2020-07-08 15:54:38 +0300

    add userdata to init tests

---
+++ app/src/androidTest/java/com/example/technopolis/api/UserData.java
@@ -0,0 +1,8 @@
+package com.example.technopolis.api;
+
+public class UserData {
+
+    public static String login = "****PASTE LOGIN HERE****";
+    public static String password = "****PASTE PASS HERE****";
+
+}
